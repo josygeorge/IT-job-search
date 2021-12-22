28e880aac6b1b18fc604d62f598ba4cb726dc1ad
Josy George <joe.george.josy@gmail.com>
2021-12-22 01:31:21 -0500

    backend server.js code review

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,9 @@ app.use('/api/jobs', jobsRoute);
 
 // Production config
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static('frontend/build'));
-    app.get("/*", (req, res) => {
+    app.use(express.static(path.join__dirname, 'frontend/build'));
+    //app.use('/', express.static('frontend/build'));
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
     });
 } else {
